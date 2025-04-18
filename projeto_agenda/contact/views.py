e67b32242e2dc96712d066199d78dfe4bb01566b
Guilherme Moreira <devguilhermemoreira@gmail.com>
2025-04-17 23:09:08 -0300

    ajustes na página de busca

--- projeto_agenda/contact/views.py
+++ projeto_agenda/contact/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
@@ -27,5 +28,33 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context=context
+    )
+
+def search(request):
+    search_value = request.GET.get('q','').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(phone__icontains=search_value)
+        ) \
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Busca - ',
+        'search_value': search_value
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context=context
     )
