a405c9368c6a4e7614a604d02b9bb4a1b26b3eb8
Guilherme Moreira <devguilhermemoreira@gmail.com>
2025-04-21 19:53:27 -0300

    adicionando paginação

--- projeto_agenda/contact/views.py
+++ projeto_agenda/contact/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -47,8 +52,12 @@ def search(request):
         ) \
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Busca - ',
         'search_value': search_value
     }
